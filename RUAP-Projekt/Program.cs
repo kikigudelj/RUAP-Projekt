ce6e57072193988446769b303160e7ba4e328f86
Kristijan Gudelj <kiki.gudelj5@gmail.com>
2024-11-08 15:28:50 +0100

    First commit

---
+++ RUAP-Projekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RUAP_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Kristijana Gudelj");
+        }
+    }
+}
